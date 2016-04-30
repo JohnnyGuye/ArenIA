13f83d5f5d880d2caf4a85d7cf5d5e643615b6ec
Simon Belletier <simon.belletier@etu.univ-amu.fr>
2016-04-30 15:15:36 +0200

    Test hitbox
    Hitbox prend maintenant l'adresse du vecteur position (passage en
    pointeur) l'interface n'a normalement pas changée.

--- Prototypes/Base Ogre/Ogre1Test/Hitbox.h
+++ Prototypes/Base Ogre/Ogre1Test/Hitbox.h
@@ -17,16 +17,15 @@ public:
 
 	inline Hitbox( Ogre::Vector3 position, Ogre::Vector3 offset = Ogre::Vector3::ZERO, BoxType boxType = EMPTY)
 		: boxType_(boxType),
-		position_(new Ogre::Vector3(position)),
 		offset_(offset)
 	{
+		position_ = &position ;
 	}
 
-	inline virtual ~Hitbox(void)
-	{}
+	inline virtual ~Hitbox(void){}
 
 	inline virtual Ogre::Vector3 getCenter() {	return *position_ + offset_;	}
-	inline virtual Ogre::Vector3* getPosition() const	{	return position_;	}
+	inline virtual Ogre::Vector3 getPosition()	{	return *position_;	}
 	inline virtual Ogre::Vector3 getOffset() const	{	return offset_;	}
 
 	virtual bool intersect( Hitbox* hitbox) const = 0;
