893872bcf8e3d07e6ebfa343a39b553e024cb6cf
Simon Belletier <simon.belletier@etu.univ-amu.fr>
2016-05-24 12:26:07 +0200

    petite methode pour check si un robot est un allié
    isAnAlly -> pas très utile pour l'instant mais elle est virtual donc
    pour des futurs modes de jeu ça peut être pas mal

--- Prototypes/Base Ogre/Ogre1Test/Robot.cpp
+++ Prototypes/Base Ogre/Ogre1Test/Robot.cpp
@@ -303,3 +303,8 @@ std::list<GameObject*> Robot::getSeenObjects(bool fetchRobots, bool fetchMissile
 
 	return seenObjects;
 }
+
+bool Robot::isAnAlly(Robot* r) const
+{
+	return getTeam() == r->getTeam();
+}
